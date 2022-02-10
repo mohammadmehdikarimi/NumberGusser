71dd33f4007ebfcbee8b0fca7aa0eb179347cdc6
mohammadmehdikarimi <karimimohammadmehdi99@gmail.com>
2022-02-10 06:42:30 -0800

    change code block in compareGuesses function

--- script.js
+++ script.js
@@ -7,40 +7,23 @@ const generateTarget = () => Math.floor(Math.random() * 10);
 
 //function will be called each round to determine which guess is closest to the target number
 const compareGuesses = (userGuess, computerGuess, secretNumber) => {
-    if (secretNumber < userGuess && secretNumber < computerGuess) {
-        if (userGuess < computerGuess) {
-            return true;
-        } else {
-            return false;
-        }
-    } else if (userGuess < secretNumber < computerGuess) {
-        if (secretNumber - userGuess === computerGuess - secretNumber) {
-            return true;
-        } else if (secretNumber - userGuess < computerGuess - secretNumber) {
-            return true;
-        } else {
-            return false;
-        }
-    } else if (userGuess < secretNumber && computerGuess < secretNumber) {
-        if (userGuess > computerGuess) {    
-            return true;
-        } else {
-            return false;
-        }
-    } else if (userGuess === secretNumber) {
-        return true;
-    } else if (computerGuess === secretNumber) {
-        return false;
-    } 
-}
-// updates score of one player based on how is the winner 
+  if (Math.abs(userGuess - secretNumber) === Math.abs(computerGuess - secretNumber)) {
+      return true;
+  } else if (Math.abs(userGuess - secretNumber) < Math.abs(computerGuess - secretNumber)) {
+      return true;
+  } else {
+      return false;
+  }
+};
+
+// updates score of one player based on how is the winner
 const updateScore = (announceWinner) => {
-    if (announceWinner === 'human') {
-        humanScore += 1; 
-    } else {
-        computerScore += 1;
-    }
-}
+  if (announceWinner === "human") {
+    humanScore += 1;
+  } else {
+    computerScore += 1;
+  }
+};
 
 // update the round number after each round
-const advanceRound = () => currentRoundNumber += 1;
+const advanceRound = () => (currentRoundNumber += 1);
