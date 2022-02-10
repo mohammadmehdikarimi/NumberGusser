56980e7fff184772b4537db4ddde69109cf09da8
mohammadmehdikarimi <karimimohammadmehdi99@gmail.com>
2022-02-10 12:26:20 -0800

    add getAbsoluteDistance function

--- script.js
+++ script.js
@@ -5,25 +5,52 @@ let currentRoundNumber = 1;
 // generate random number at the start of each new round
 const generateTarget = () => Math.floor(Math.random() * 10);
 
+
+// calculate distance between user and computer entry number
+const getAbsoluteDistance = (number1, number2) => {
+  return Math.abs(number1 - number2);
+};
+
 //function will be called each round to determine which guess is closest to the target number
 const compareGuesses = (userGuess, computerGuess, targetGuess) => {
-  if (Math.abs(userGuess - targetGuess) === Math.abs(computerGuess - targetGuess)) {
-      return true;
-  } else if (Math.abs(userGuess - targetGuess) < Math.abs(computerGuess - targetGuess)) {
-      return true;
-  } else {
-      return false;
+  // calculate distance with getAbsoluteDistance function
+  let userDistance = getAbsoluteDistance(userGuess, targetGuess);
+  let computerDistance = getAbsoluteDistance(computerGuess, targetGuess);
+
+  if (userDistance === computerDistance) {
+    return true;
+  } else if (userDistance < computerDistance) {
+    return true;
+  } else if (userDistance > computerDistance) {
+    return false;
   }
+
+  /******************************************************** */
+  // calculate distance without getAbsoluteDistance function
+  /******************************************************** */
+  /*
+  if (
+    Math.abs(userGuess - targetGuess) === Math.abs(computerGuess - targetGuess)
+  ) {
+    return true;
+  } else if (
+    Math.abs(userGuess - targetGuess) < Math.abs(computerGuess - targetGuess)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+  */
 };
 
 // updates score of one player based on how is the winner
-const updateScore = announceWinner => {
+const updateScore = (announceWinner) => {
   if (announceWinner === "human") {
-    humanScore ++;
-  } else if (winner === 'computer') {
-    computerScore ++;
+    humanScore++;
+  } else if (announceWinner === "computer") {
+    computerScore++;
   }
 };
 
 // update the round number after each round
-const advanceRound = () => currentRoundNumber ++;
+const advanceRound = () => currentRoundNumber++;
